d3ee5e2df18f5a8ac03c149b8d8386dfb90c1fdb
natalialexandra <natalia.alexandra195@gmail.com>
2021-01-03 00:25:38 +0100

    add data

---
+++ src/quotes-data.js
@@ -0,0 +1,12 @@
+const quotes = [
+    "Everyone thinks of changing the world, but no one thinks of changing himself.",
+    "Respect was invented to cover the empty place where love should be.",
+    "We can know only that we know nothing. And that is the highest degree of human wisdom.",
+    "Spring is the time of plans and projects.",
+    "When you love someone, you love the person as they are, and not as you'd like them to be.",
+    "The two most powerful warriors are patience and time.",
+    "Anything is better than lies and deceit!",
+    "There is no greatness where there is not simplicity, goodness, and truth."
+];
+
+export default quotes;
