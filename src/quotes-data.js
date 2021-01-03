81ac0e9741f3bc5ef662061d0ac368c7c4cb7d8c
natalialexandra <natalia.alexandra195@gmail.com>
2021-01-03 19:20:24 +0100

    quotes

--- src/quotes-data.js
+++ src/quotes-data.js
@@ -6,7 +6,12 @@ const quotes = [
     "When you love someone, you love the person as they are, and not as you'd like them to be.",
     "The two most powerful warriors are patience and time.",
     "Anything is better than lies and deceit!",
-    "There is no greatness where there is not simplicity, goodness, and truth."
+    "There is no greatness where there is not simplicity, goodness, and truth.",
+    "Be yourself; everyone else is already taken.",
+    "Be the change that you wish to see in the world.",
+    "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+    "If you tell the truth, you don't have to remember anything.",
+    "A friend is someone who knows all about you and still loves you."
 ];
 
 export default quotes;
